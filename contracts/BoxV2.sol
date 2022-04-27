a8a073c841e677d21007eb05881cf8399b2e65be
Ritesh gurjar <gurjarritesh155@gmail.com>
2022-04-27 15:57:55 +0530

    first commit

---
+++ contracts/BoxV2.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract BoxV2 {
+    uint256 private value;
+
+    event ValueChanged(uint newValue);
+
+    function store(uint256 newValue) public{
+        value =newValue;
+        emit ValueChanged(newValue);
+    }
+    function retrive() public view returns (uint256) {
+        return value;
+    }
+    function increment() public {
+        value= value +1;
+        emit ValueChanged(value);
+    }
+}
